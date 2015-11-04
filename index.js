5fb87fa2b3ca397e5372df0b4d095f25dbb3cf7d
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-04 11:37:46 -0500

    Extend base plugin

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-cloudfront'
+  name: 'ember-cli-deploy-cloudfront',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      didActivate: function(context) {
+
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
