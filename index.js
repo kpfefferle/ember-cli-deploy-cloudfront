7be226845197f0c91d7e29f18ec1ab2b8219f373
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-04 21:40:05 -0500

    Add missing comma

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
       name: options.name,
       defaultConfig: {
         objectPaths: '/index.html'
-      }
+      },
       requiredConfig: ['accessKeyId', 'secretAccessKey', 'distributionId'],
 
       didActivate: function(context) {
