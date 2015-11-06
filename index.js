1bc6a71aff257c82821fab030b462efad9973f20
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-05 21:26:27 -0500

    Remove test config logging

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ module.exports = {
         var distributionId = this.readConfig('distributionId');
         var objectPaths = this.readConfig('objectPaths');
 
-        this.log('setting for accessKeyId: `' + accessKeyId + '`');
-        this.log('setting for secretAccessKey: `' + secretAccessKey + '`');
         this.log('preparing to create invalidation for CloudFront distribution `' + distributionId + '`');
-        this.log('setting for objectPaths: `' + objectPaths + '`');
       }
     });
 
