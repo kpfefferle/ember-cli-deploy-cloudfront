5515864ce102e6798e0711f37e3065bc449906c2
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-09 17:46:37 -0500

    Add test setup

--- tests/unit/lib/cloudfront-nodetest.js
+++ tests/unit/lib/cloudfront-nodetest.js
@@ -1,4 +1,43 @@
 var assert = require('ember-cli/tests/helpers/assert');
 
 describe('cloudfront', function() {
+  var CloudFront, mockUi, cloudfrontClient, plugin, subject;
+
+  before(function() {
+    CloudFront = require('../../../lib/cloudfront');
+  });
+
+  beforeEach(function() {
+    cloudfrontClient = {
+      createInvalidation: function(params, cb) {
+        cb();
+      }
+    };
+    mockUi = {
+      messages: [],
+      write: function() {},
+      writeLine: function(message) {
+        this.messages.push(message);
+      }
+    };
+    plugin = {
+      ui: mockUi,
+      readConfig: function(propertyName) {
+        if (propertyName === 'cloudfrontClient') {
+          return cloudfrontClient;
+        }
+      },
+      log: function(message, opts) {
+        this.ui.write('|    ');
+        this.ui.writeLine('- ' + message);
+      }
+    };
+    subject = new CloudFront({
+      plugin: plugin
+    });
+  });
+
+  describe('#invalidate', function() {
+    
+  });
 });
